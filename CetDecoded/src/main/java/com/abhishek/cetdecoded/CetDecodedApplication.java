e4a3639739aafc1df660074505e2633b21f6e042
lightt77 <abhisheksatam17@gmail.com>
2019-04-06 23:20:44 +0530

    Add initial structure for backend

---
+++ CetDecoded/src/main/java/com/abhishek/cetdecoded/CetDecodedApplication.java
@@ -0,0 +1,15 @@
+package com.abhishek.cetdecoded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CetDecodedApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CetDecodedApplication.class, args);
+    }
+
+}
