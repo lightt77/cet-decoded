4cb2069b673bbb04bfc19f59023b42916d287292
lightt77 <abhisheksatam17@gmail.com>
2019-07-07 16:00:59 +0530

    Add base react project

---
+++ webapp-react/webapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from "./components/navbar/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Dashboard></Dashboard>
+    </div>
+  );
+}
+
+export default App;
