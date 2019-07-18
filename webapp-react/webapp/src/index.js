a34900fc71d1778e2db8db0e1de9c416e355dcb6
lightt77 <abhisheksatam17@gmail.com>
2019-07-18 22:32:27 +0530

    Fix issue with custom css changes not taking effect

--- webapp-react/webapp/src/index.js
+++ webapp-react/webapp/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './bootstrap/bootstrap-flatly.css';
+import './bootstrap/bootstrap-flatly-override.css';
+import './components/navbar/navbar.css';
+import App from './App.tsx';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
