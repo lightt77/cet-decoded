b59ab0166cde6ac68a084b1a232056d99fa9621a
lightt77 <abhisheksatam17@gmail.com>
2019-07-20 13:01:19 +0530

    Fix typo in csv loader scripts form question

--- SqlScripts/v1/initialise-database.sql
+++ SqlScripts/v1/initialise-database.sql
@@ -90,8 +90,8 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/lightt77/abhishek/coding/open source/myprojects/cet-decoded/SqlScripts/v1/csvs/Prose.csv' 
-INTO TABLE Prose
+LOAD DATA LOCAL INFILE '/home/lightt77/abhishek/coding/open source/myprojects/cet-decoded/SqlScripts/v1/csvs/Question.csv' 
+INTO TABLE Question
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
