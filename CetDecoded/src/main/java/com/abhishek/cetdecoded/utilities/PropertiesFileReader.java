55fda89946cfd9f9178f3628ed149eb722cab464
lightt77 <abhisheksatam17@gmail.com>
2019-09-01 23:12:09 +0530

    Remove hardcoded properties file path

--- CetDecoded/src/main/java/com/abhishek/cetdecoded/utilities/PropertiesFileReader.java
+++ CetDecoded/src/main/java/com/abhishek/cetdecoded/utilities/PropertiesFileReader.java
@@ -2,15 +2,26 @@ package com.abhishek.cetdecoded.utilities;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Properties;
 
 public class PropertiesFileReader
 {
     private static Properties properties;
-    private final static String PROPERTIES_FILE_PATH = "src/main/resources/application.properties";
+    //private final static String PROPERTIES_FILE_PATH = "src/main/resources/application.properties";
+    private static String PROPERTIES_FILE_PATH;
 
     static
     {
+        try
+        {
+            PROPERTIES_FILE_PATH = PropertiesFileReader.class.getClassLoader().getResource("application.properties").toURI().getPath();
+        }
+        catch (URISyntaxException e)
+        {
+            e.printStackTrace();
+        }
+
         properties = new Properties();
         try
         {
