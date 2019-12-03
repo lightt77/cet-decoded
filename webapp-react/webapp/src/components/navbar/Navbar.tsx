cb2580134f273f1f20432d5acb1c9acdde430d97
lightt77 <abhisheksatam17@gmail.com>
2019-12-03 20:14:09 +0530

    Change CetDecoded to ContentCract in UI

--- webapp-react/webapp/src/components/navbar/Navbar.tsx
+++ webapp-react/webapp/src/components/navbar/Navbar.tsx
@@ -34,7 +34,7 @@ export default class Navbar extends Component<any, IState> {
     return (
       <div className="Navbar">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a id="navbarBrandName" className="navbar-brand" href="/">CetDecoded</a>
+          <a id="navbarBrandName" className="navbar-brand" href="/">ContentCrack</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
